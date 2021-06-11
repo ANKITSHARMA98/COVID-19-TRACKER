bfb28fc20bdd53bca833382a7a10d584bc80f439
Ankit <ankitsharma8794@gmail.com>
2021-06-11 20:25:40 +0530

    Updates

---
+++ precache-manifest.0893fb060004c1df4830cded2aaa3f13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1badaebcab668ec22e7535d1d1de0843",
+    "url": "/COVID-19-TRACKER/index.html"
+  },
+  {
+    "revision": "98e6acd6784ab4292b37",
+    "url": "/COVID-19-TRACKER/static/css/2.f7023ff7.chunk.css"
+  },
+  {
+    "revision": "5db0c474e668def9c9ea",
+    "url": "/COVID-19-TRACKER/static/css/main.889e491d.chunk.css"
+  },
+  {
+    "revision": "98e6acd6784ab4292b37",
+    "url": "/COVID-19-TRACKER/static/js/2.8dfe52f6.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/COVID-19-TRACKER/static/js/2.8dfe52f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db0c474e668def9c9ea",
+    "url": "/COVID-19-TRACKER/static/js/main.26c2fa2c.chunk.js"
+  },
+  {
+    "revision": "7d2df0ca2ef4e72d1c5a",
+    "url": "/COVID-19-TRACKER/static/js/runtime-main.68e40c04.js"
+  }
+]);
